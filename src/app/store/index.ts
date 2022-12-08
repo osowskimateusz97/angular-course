2b5548d2afa5e008c1eb0df5c1cbea26fe50e8d0
Osowski, Mateusz <Mateusz.Osowski@informa.com>
2022-12-09 00:45:26 +0100

    feat: add missed auth reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,7 +1,11 @@
 import { ActionReducerMap } from '@ngrx/store';
+import { authReducer } from '../auth/store/auth.reducer';
 import { userReducer } from '../user/store/user.reducer';
 
 interface State {}
 
-export const reducers: ActionReducerMap<State> = { user: userReducer };
+export const reducers: ActionReducerMap<State> = {
+  user: userReducer,
+  auth: authReducer,
+};
 export const effects = [];
